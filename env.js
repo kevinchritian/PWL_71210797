793cc128ec531a1f077c82158fbe02f110cb23ae
Kevin Christian -71210797 <93351620+kevinchritian@users.noreply.github.com>
2023-10-01 19:54:39 +0700

    71210797

---
+++ env.js
@@ -0,0 +1,14 @@
+// 1. Membuat Env Variable sesuai data yang diregister
+const cat_env = {
+  API_KEY: "live_akvvIruVBWjHUZd5nb4wbN9dWbuusRQXu0udOJRnnPW6KNWyjGJVUrSDwIWI5q5A",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+const dog_env = {
+  API_KEY: "live_akvvIruVBWjHUZd5nb4wbN9dWbuusRQXu0udOJRnnPW6KNWyjGJVUrSDwIWI5q5A",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+// 2. Export dog_env
+export { cat_env, dog_env};
+
